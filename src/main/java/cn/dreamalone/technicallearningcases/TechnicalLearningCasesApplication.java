382626c4e46d07ac3076d93e484d445ecf2308a8
leitu <leitu@x2era.com>
2020-10-15 14:42:49 +0800

    Technical learning Cases

---
+++ src/main/java/cn/dreamalone/technicallearningcases/TechnicalLearningCasesApplication.java
@@ -0,0 +1,13 @@
+package cn.dreamalone.technicallearningcases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalLearningCasesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalLearningCasesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/dreamalone/technicallearningcases/TechnicalLearningCasesApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.dreamalone.technicallearningcases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalLearningCasesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
